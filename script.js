0432e70e3996d024f94976cec89efb846060dd55
striver2001 <111413617+striver2001@users.noreply.github.com>
2023-07-30 18:03:56 +0530

    added succesfully

---
+++ script.js
@@ -0,0 +1,16 @@
+async function shortenURL () {
+    const url = document.getElementById("url").value;
+    const response = await fetch(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`);
+    console.log(response);
+
+    if (response.ok)
+    {
+        const data = await response.text();
+        document.getElementById('result').innerHTML = ` 
+        Your shortened URL : <a href = "${data}" target="_blank">${data}</a>`;
+    }
+    else {
+        document.getElementById('result').innerHTML = "Error in Shortening URL";
+    }
+
+}
